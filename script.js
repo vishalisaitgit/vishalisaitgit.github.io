d0fdbddee729355b274098effab473c83670fd8d
vishalisaitgit <115639913+vishalisaitgit@users.noreply.github.com>
2023-08-02 21:27:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ names[8] = "laura";
 names[9] = "jim";
 
 for (var i = 0; i < names.length; i++) {
-  if (names[i].charAt(0) === "J" || names[i].charAt(0) === "j") {
+  if (names[i].charAt(0) === 'J' || names[i].charAt(0) === 'j') 
+  {
     console.log("Goodbye" + names[i]);
-  } else {
+  } 
+  else 
+  {
     console.log("Hello" + names[i]);
   }
 }
